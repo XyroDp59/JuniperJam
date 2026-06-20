5c50e244ae20a4ffca1a289b7383455d6d03ee1c
SwiloPlt <corentin.pichon.92@gmail.com>
2026-06-20 18:54:51 +0200

    fix: "Children" remplace "FirstEnnemi"

--- JuniperJamProject/Assets/Scripts/ChildrenScript.cs
+++ JuniperJamProject/Assets/Scripts/ChildrenScript.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class FirstEnnemiScript : MonoBehaviour
+public class ChildrenScript : MonoBehaviour
 {
     [SerializeField] private float speed = 3;
     [SerializeField] private GameObject player;
