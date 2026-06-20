3100e1634481453563d5f764e2e9e8a3606d7e88
Raton-mng <149146535+Raton-mng@users.noreply.github.com>
2026-06-20 14:11:56 -0400

    feat: Dash for Player (and no more jump) and Universal Attribute Set

---
+++ JuniperJamProject/Assets/Character/AttributSet.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AttributSet : MonoBehaviour
+{
+    [SerializeField] private int maxHp;
+    private int currentHp;
+    public int CurrentHp
+    {
+        get {  return currentHp; }
+        set { 
+            value = Mathf.Clamp(value, 0, maxHp);
+            
+            onHpChange.Invoke(value, ((float)value) / maxHp); 
+            if (value <= 0) onDeath.Invoke();
+            
+            currentHp = value;
+        }
+    }
+
+    public UnityEvent<int, float> onHpChange; // <currentHp, currentPourcentage> (post change) 
+    public UnityEvent onDeath;
+
+    private void Awake()
+    {
+        CurrentHp = maxHp;
+    }
+}
