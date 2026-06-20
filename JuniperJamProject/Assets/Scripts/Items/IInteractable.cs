e2495db7768305cdaa9c192e92ead0df0f6d7d6d
EliasDelPozo <delpozo.elias59@gmail.com>
2026-06-20 14:26:37 -0400

    wip: created scripts for yoyo weapon

---
+++ JuniperJamProject/Assets/Scripts/Items/IInteractable.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public interface IInteractable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Interact();
+
+}
