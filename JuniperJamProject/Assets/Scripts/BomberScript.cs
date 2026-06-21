2e4734ab3ad4c4cc4424ef54a36e63e4311f933a
SwiloPlt <corentin.pichon.92@gmail.com>
2026-06-21 12:40:44 +0200

    feat: bomb prefab
    the bomb doesn't have a script yet, but i made one for the bomber (they can spawn bomb now)

--- JuniperJamProject/Assets/Scripts/BomberScript.cs
+++ JuniperJamProject/Assets/Scripts/BomberScript.cs
@@ -4,19 +4,24 @@ public class BomberScript : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private SpawnerScript bomberSpawner;
+    [SerializeField] private float bomberPathRadius;
     [SerializeField] private float maxY;
     [SerializeField] private float minY;
     [SerializeField] private float speedY;
     [SerializeField] private float timeOfFly;
+    [SerializeField] private GameObject bomb;
+    [SerializeField] private float bombRate;
+    [SerializeField] private float bombSpeed;
+    [SerializeField] private float explosionTime;
 
     private bool isDescending = false;
     private bool isAscending = false;
     private float timer = 0;
+    private float lastTimeBomb = 0;
     private float angle;
 
     void Awake()
     {
-        Debug.Log(transform.position);
         timer = 0;
         isDescending = true;
         isAscending = false;
@@ -34,16 +39,22 @@ public class BomberScript : MonoBehaviour
             {
                 isAscending = true;
             }
-            transform.position = new Vector3(Mathf.Cos(angle + timer) * bomberSpawner.getCarrouselRadius(),
+            transform.position = new Vector3(cos(angle + timer),
                                             transform.position.y,
-                                            Mathf.Sin(angle + timer) * bomberSpawner.getCarrouselRadius());
+                                            sin(angle + timer));
+            if (timer - lastTimeBomb > bombRate)
+            {
+                lastTimeBomb = timer;
+                Instantiate(bomb,
+                            new Vector3(transform.position.x, transform.position.y - (1/2), transform.position.z),
+                            transform.rotation);
+            }
         }
         else if (isDescending)
         {
-            Debug.Log(maxY - (timer * speedY));
-            transform.position = new Vector3(Mathf.Cos(angle + timer) * bomberSpawner.getCarrouselRadius(),
+            transform.position = new Vector3(cos(angle + timer),
                                             maxY - timer * speedY,
-                                            Mathf.Sin(angle + timer) * bomberSpawner.getCarrouselRadius());
+                                            sin(angle + timer));
             if (transform.position.y < minY)
             {
                 isDescending = false;
@@ -53,14 +64,24 @@ public class BomberScript : MonoBehaviour
         }
         else if (isAscending)
         {
-            transform.position = new Vector3(Mathf.Cos(angle + timer) * bomberSpawner.getCarrouselRadius(),
-                                            minY + (timer - timeOfFly)*speedY,
-                                            Mathf.Sin(angle + timer) * bomberSpawner.getCarrouselRadius());
+            transform.position = new Vector3(cos(angle + timer),
+                                            minY + (timer - timeOfFly) * speedY,
+                                            sin(angle + timer));
             if (transform.position.y > maxY)
             {
                 Destroy(gameObject);
             }
         }
+    }
+
+    private float cos(float angle)
+    {
+        return Mathf.Cos(angle) * bomberPathRadius;
+    }
+
+    private float sin(float angle)
+    {
+        return Mathf.Sin(angle) * bomberPathRadius;
     }
 
     public float getMaxY()
