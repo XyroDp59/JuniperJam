1b09a4618c77ffdf8e05a28a504c8059ca0e8512
Raton-mng <149146535+Raton-mng@users.noreply.github.com>
2026-06-23 16:42:04 -0400

    feat: A yoyo following the player inputs

--- JuniperJamProject/Assets/Scripts/Items/BaseMovingItem.cs
+++ JuniperJamProject/Assets/Scripts/Items/BaseMovingItem.cs
@@ -2,41 +2,46 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public abstract class BaseMovingItem : Item
+public abstract class BaseMovingItem : MonoBehaviour
 {
-    private bool isKeyboardOrMouse = false;
+    [HideInInspector] public PlayerScript player;
+    
+    protected bool isKeyboardOrMouse = true;
     private Camera cam;
-    private Mouse mouse;
     private Vector2 moveInput;
 
     private void OnEnable()
     {
         player.controls.Player.Move.performed += (context) => {
-            if ( context.control.device is Keyboard or Mouse ) {isKeyboardOrMouse = true;
-                mouse = Mouse.current;
-            }
-            else isKeyboardOrMouse = false;
+            if ( context.control.device is Keyboard or Mouse ) isKeyboardOrMouse = true;
+            else if (context.ReadValue<Vector2>() != Vector2.zero)
+                isKeyboardOrMouse = false;
         };
-        player.controls.Player.ItemControl.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        
+        player.controls.Player.ItemControl.performed += ctx =>
+        {
+            moveInput = ctx.ReadValue<Vector2>();
+            if (moveInput != Vector2.zero) 
+                isKeyboardOrMouse = false;
+        };
         player.controls.Player.ItemControl.canceled += ctx => moveInput = Vector2.zero;
         
         cam = Camera.main; //ou main camera ?
-        
-        player.controls.Player.Attack.performed += ctx => GetDirection();
     }
 
-    private Vector2 GetDirection()
+    protected Vector2 GetInput()
     {
         if (isKeyboardOrMouse)
         {
-            Ray ray = cam.ScreenPointToRay(mouse.position.ReadValue()); // a optimiser
-            Physics.Raycast(ray, out RaycastHit hit);
-            print (hit.point);
-            Vector3 dir = hit.point - transform.position;
-            return new Vector2(dir.x, dir.z).normalized; 
+            Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue()); // a optimiser
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            plane.Raycast(ray, out float hit);
+            Vector3 point = ray.GetPoint(hit);
+            return new Vector2(point.x, point.z);
+            //Vector3 dir = hit.point - transform.position;
+            //return new Vector2(dir.x, dir.z).normalized; 
         }
         
-        print(moveInput);
         return moveInput;
     }
 }
