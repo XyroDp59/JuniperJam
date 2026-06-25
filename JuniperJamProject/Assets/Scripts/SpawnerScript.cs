6252ff9afb1240abcb41af5d35e0937e063d1ea9
SwiloPlt <corentin.pichon.92@gmail.com>
2026-06-25 12:12:01 +0200

    feat: nouveau système de spawn de mobs
    détails du système sur discord

--- JuniperJamProject/Assets/Scripts/SpawnerScript.cs
+++ JuniperJamProject/Assets/Scripts/SpawnerScript.cs
@@ -7,40 +7,39 @@ public class SpawnerScript : MonoBehaviour
 {
 
     [SerializeField] private EnnemiClassScript ennemi;
-    [SerializeField] private float spawnRate = 5;
     [SerializeField] private float carrouselRadius = 8;
     [SerializeField] private int nombreSpawnPossible = 1000;
     [SerializeField] private float distanceMinFromPlayer = 5;
     [SerializeField] private ScoreManager _scoreManager;
+    [SerializeField] public int timeBeforeCanSpawn;
+    [SerializeField] public int initialSpawnPercentage = 0;
+    [SerializeField] public int finalSpawnPercentage = 0;
+    [SerializeField] public int spawnPercentageChange;
+    [SerializeField] public MobSpawnerScript mobSpawner;
 
     private GameObject player;
-    private float timer = 0;
+    private int spawnPercentage;
     private float angle;
 
     private void Start()
     {
         _scoreManager = _scoreManager.GetComponent<ScoreManager>();
         Debug.Log(_scoreManager);
+        mobSpawner = mobSpawner.GetComponent<MobSpawnerScript>();
+        player = mobSpawner.GetPlayer();
+        spawnPercentage = initialSpawnPercentage;
     }
 
-    void Update()
+    public void spawn()
     {
-        if (timer < spawnRate)
-        {
-            timer += Time.deltaTime;
-        }
-        else
-        {
-            timer = 0;
-            randomAngle();
-            Debug.Log(Vector3.Distance(spawn(), player.transform.position));
-            EnnemiClassScript ennemiInstance = Instantiate(ennemi, spawn(), transform.rotation);
-            ennemiInstance.attributSet.onDeath.AddListener(() => { ennemiInstance.gameObject.SetActive(false); });
-            ennemiInstance.attributSet.onDeath.AddListener(() => { _scoreManager.IncrementScore(ennemiInstance.rewardScore); }); //jsp pk on peut pas le faire depuis l'ennemi ca
-        }
+        randomAngle();
+        Debug.Log(Vector3.Distance(spawnPosition(), player.transform.position));
+        EnnemiClassScript ennemiInstance = Instantiate(ennemi, spawnPosition(), transform.rotation);
+        ennemiInstance.attributSet.onDeath.AddListener(() => { ennemiInstance.gameObject.SetActive(false); });
+        ennemiInstance.attributSet.onDeath.AddListener(() => { _scoreManager.IncrementScore(ennemiInstance.rewardScore); }); //jsp pk on peut pas le faire depuis l'ennemi ca
     }
 
-    private Vector3 spawn()
+    private Vector3 spawnPosition()
     {
         Vector3 spawnPosition = new Vector3(math.cos(angle) * carrouselRadius, 0, math.sin(angle) * carrouselRadius);
         return spawnPosition;
@@ -49,7 +48,7 @@ public class SpawnerScript : MonoBehaviour
     private void randomAngle()
     {
         angle = UnityEngine.Random.Range(0, nombreSpawnPossible) * 2 * math.PI / nombreSpawnPossible;
-        while (Vector3.Distance(spawn(), player.transform.position) < distanceMinFromPlayer)
+        while (Vector3.Distance(spawnPosition(), player.transform.position) < distanceMinFromPlayer)
         {
             angle = UnityEngine.Random.Range(0, nombreSpawnPossible) * 2 * math.PI / nombreSpawnPossible;
         }
@@ -70,5 +69,25 @@ public class SpawnerScript : MonoBehaviour
         {
             player = other.gameObject;
         }
+    }
+
+    public bool getCanSpawn(float time)
+    {
+        return (timeBeforeCanSpawn <= time);
+    }
+
+    public int getSpawnPercentage()
+    {
+        return spawnPercentage;
+    }
+
+    public void changeSpawnPercentage()
+    {
+        spawnPercentage = spawnPercentage + spawnPercentageChange;
+    }
+
+    public int getFinalSpawnPercentage()
+    {
+        return finalSpawnPercentage;
     }
 }
