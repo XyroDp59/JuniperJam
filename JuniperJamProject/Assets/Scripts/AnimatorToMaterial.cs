33ee53d6ce7ab0007a04fae512f193ef02f21247
XyroDp59 <81575508+XyroDp59@users.noreply.github.com>
2026-06-25 13:20:51 -0400

    feat: moved to URP and fixed transparency

--- JuniperJamProject/Assets/Scripts/AnimatorToMaterial.cs
+++ JuniperJamProject/Assets/Scripts/AnimatorToMaterial.cs
@@ -1,44 +1,55 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering.HighDefinition;
 
 public class AnimatorToMaterial : MonoBehaviour
 {
-    private static readonly int DoubleSidedEnable = Shader.PropertyToID("_DoubleSidedEnable");
-    private static readonly int AlphaCutoff = Shader.PropertyToID("_AlphaCutoff");
-    private static readonly int AlphaCutoffEnable = Shader.PropertyToID("_AlphaCutoffEnable");
+   /* // URP/Lit property IDs
+    private static readonly int Cull = Shader.PropertyToID("_Cull");
+    private static readonly int AlphaClip = Shader.PropertyToID("_AlphaClip");
+    private static readonly int Cutoff = Shader.PropertyToID("_Cutoff");
+    private static readonly int Surface = Shader.PropertyToID("_Surface");
+    private static readonly int SrcBlend = Shader.PropertyToID("_SrcBlend");
+    private static readonly int DstBlend = Shader.PropertyToID("_DstBlend");
+    private static readonly int ZWrite = Shader.PropertyToID("_ZWrite");*/
+
     [SerializeField] MeshRenderer meshRenderer;
     [SerializeField] SpriteRenderer spriteRenderer;
-    Material _material;
+
     [SerializeField] float widthImage;
     [SerializeField] float heightImage;
     [SerializeField] float widthSprite;
     [SerializeField] float heightSprite;
     [SerializeField] float xBaseOffset;
     [SerializeField] float yBaseOffset;
-    
+
     private float _spinRemainingDuration = 0;
     private float _spinTotalDuration = 0;
     private int _numberOfSpin = 1;
 
+    [SerializeField] Material referenceMaterial;
+    Material _material;
+
     private void Awake()
     {
-        _material = new Material(Shader.Find("HDRP/Lit"));
-        _material.SetFloat(DoubleSidedEnable, 1f);
-        _material.SetFloat(AlphaCutoffEnable, 1f);
-        _material.SetFloat(AlphaCutoff, 0.5f);
+        _material = new Material(referenceMaterial);
+
         _material.mainTexture = spriteRenderer.sprite.texture;
         _material.mainTextureScale = new Vector2(widthSprite / widthImage, heightSprite / heightImage);
-        HDMaterial.ValidateMaterial(_material);
+
         meshRenderer.material = _material;
     }
 
     void Update()
     {
-        _material.mainTextureOffset = new Vector2((xBaseOffset + spriteRenderer.sprite.rect.x) / widthImage, (yBaseOffset + spriteRenderer.sprite.rect.y) / heightImage);
+        _material.mainTextureOffset = new Vector2(
+            (xBaseOffset + spriteRenderer.sprite.rect.x) / widthImage,
+            (yBaseOffset + spriteRenderer.sprite.rect.y) / heightImage
+        );
+
         if (_spinRemainingDuration > 0)
         {
-            transform.RotateAround(transform.position, Vector3.up, - Time.deltaTime * 360 * _numberOfSpin / _spinTotalDuration);
+            transform.RotateAround(transform.position, Vector3.up,
+                -Time.deltaTime * 360 * _numberOfSpin / _spinTotalDuration);
             _spinRemainingDuration -= Time.deltaTime;
         }
     }
@@ -50,7 +61,6 @@ public class AnimatorToMaterial : MonoBehaviour
             _spinRemainingDuration = duration;
             _spinTotalDuration = duration;
             _numberOfSpin = numberOfSpin;
-
             StartCoroutine(EndSpin());
         }
     }
@@ -58,7 +68,6 @@ public class AnimatorToMaterial : MonoBehaviour
     private IEnumerator EndSpin()
     {
         yield return new WaitForSeconds(_spinTotalDuration + 0.05f);
-        
         transform.rotation = Quaternion.LookRotation(Vector3.down, Vector3.back);
     }
 }
