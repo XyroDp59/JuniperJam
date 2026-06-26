d710116cf59c813d488e70a55edef24cc12bfe49
Raton-mng <149146535+Raton-mng@users.noreply.github.com>
2026-06-26 11:42:22 -0400

    feat: End screen

---
+++ JuniperJamProject/Assets/Scripts/UI/EndScreen.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject mainUi;
+    [SerializeField] private Image panel;
+    [SerializeField] private TextMeshProUGUI endText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private GameObject buttons;
+    [SerializeField] private AttributSet playerAttributeSet;
+
+    private void Awake()
+    {
+        playerAttributeSet.onDeath.AddListener(() => StartCoroutine(EndGame()));
+    }
+
+    private IEnumerator EndGame()
+    {
+        mainUi.SetActive(false);
+        Time.timeScale = 0.5f;
+
+        var panelColor = panel.color;
+        panelColor.a = 0;
+        panel.color = panelColor;
+        panel.gameObject.SetActive(true);
+        float currentShade = 0;
+        while (currentShade < 0.125f)
+        {
+            print (panel.color + "; " + currentShade);
+            currentShade += 0.0025f;
+            var color = panel.color;
+            color.a = currentShade;
+            panel.color = color;
+            yield return new WaitForSeconds(0.02f);
+        }
+        Time.timeScale = 0;
+        endText.gameObject.SetActive(true);
+        //yield return new WaitForSeconds(1f);
+        scoreText.text = "For your effort, you achieved to recieve " + ScoreManager.Instance.GetScore() + " dollars.";
+        scoreText.gameObject.SetActive(true);
+        //yield return new WaitForSeconds(1f);
+        buttons.SetActive(true);
+    }
+    
+    public void ExitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        
+        // SFX
+        //FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Pause", 0);
+    }
+    
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("GameScene");
+        
+        // SFX
+        //FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Pause", 0);
+    }
+}
